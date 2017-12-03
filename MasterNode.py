76ee254952e1c8e960457a99dc016e7084f09a82
kzhangair <kzhangair@qq.com>
2017-12-03 15:08:55 +0800

    threading support

--- MasterNode.py
+++ MasterNode.py
@@ -4,6 +4,8 @@ import socket
 import sys
 import struct
 import os
+import threading
+import time
 
 import name_node
 
@@ -97,15 +99,62 @@ def SortKey(s):
     k2, value2 = line.split()
     return k2
 
+class RunRemotePyScriptThread (threading.Thread):   #继承父类threading.Thread
+    def __init__(self, name, addr, port, script_name, script_input, script_result):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.addr = addr
+        self.port = port
+        self.script_name = script_name
+        self.script_input = script_input
+        self.script_result = script_result
+    def run(self):                   #把要执行的代码写到run函数里面 线程在创建后会直接运行run函数
+        print "Starting thread " + self.name
+        RunRemotePyScript(self.addr, self.port, self.script_name, self.script_input, self.script_result)
+        print "Exiting thread " + self.name
+
+class UploadFileThread(threading.Thread):
+    def __init__(self, name, addr, port, file_name):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.addr = addr
+        self.port = port
+        self.file_name = file_name
+    def run(self):
+        print "Starting thread " + self.name
+        UploadFile(self.addr, self.port, self.file_name)
+        print "Exiting thread " + self.name
+
 def MyMapReduce(mapper, reducer, DFSInputFile, DFSOutputFile):
+    start = time.clock()
     #prepare mapper input file
-    UploadFile("thumm02", 31728, mapper)
-    UploadFile("thumm03", 31728, mapper)
-    UploadFile("thumm04", 31728, mapper)
+    #UploadFile("thumm02", 31728, mapper)
+    #UploadFile("thumm03", 31728, mapper)
+    #UploadFile("thumm04", 31728, mapper)
+    upload_t2 = UploadFileThread("upload_t2", "thumm02", 31728, mapper)
+    upload_t3 = UploadFileThread("upload_t3", "thumm03", 31728, mapper)
+    upload_t4 = UploadFileThread("upload_t4", "thumm04", 31728, mapper)
+    upload_t2.start()
+    upload_t3.start()
+    upload_t4.start()
+    upload_t2.join()
+    upload_t3.join()
+    upload_t4.join()
+
     print "mapper send complete"
-    UploadFile("thumm02", 31728, reducer)
-    UploadFile("thumm03", 31728, reducer)
-    UploadFile("thumm04", 31728, reducer)
+    #UploadFile("thumm02", 31728, reducer)
+    #UploadFile("thumm03", 31728, reducer)
+    #UploadFile("thumm04", 31728, reducer)
+    upload_t2 = UploadFileThread("upload_t2", "thumm02", 31728, reducer)
+    upload_t3 = UploadFileThread("upload_t3", "thumm03", 31728, reducer)
+    upload_t4 = UploadFileThread("upload_t4", "thumm04", 31728, reducer)
+    upload_t2.start()
+    upload_t3.start()
+    upload_t4.start()
+    upload_t2.join()
+    upload_t3.join()
+    upload_t4.join()
+
     print "reducer send complete"
     name_node.DFSLoad(DFSInputFile, "MapInput")
     finput = open("MapInput", 'r')
@@ -128,17 +177,37 @@ def MyMapReduce(mapper, reducer, DFSInputFile, DFSOutputFile):
     finput_03.close()
     finput_04.close()
     print "begin send MapInput"
-    UploadFile("thumm02", 31728, "MapInput_02")
-    UploadFile("thumm03", 31728, "MapInput_03")
-    UploadFile("thumm04", 31728, "MapInput_04")
+    #UploadFile("thumm02", 31728, "MapInput_02")
+    #UploadFile("thumm03", 31728, "MapInput_03")
+    #UploadFile("thumm04", 31728, "MapInput_04")
+    upload_t2 = UploadFileThread("upload_t2", "thumm02", 31728, "MapInput_02")
+    upload_t3 = UploadFileThread("upload_t3", "thumm03", 31728, "MapInput_03")
+    upload_t4 = UploadFileThread("upload_t4", "thumm04", 31728, "MapInput_04")
+    upload_t2.start()
+    upload_t3.start()
+    upload_t4.start()
+    upload_t2.join()
+    upload_t3.join()
+    upload_t4.join()
+
     os.remove("MapInput_02")
     os.remove("MapInput_03")
     os.remove("MapInput_04")
     print "MapInput send complete"
     #run Mapper
-    RunRemotePyScript("thumm02", 31728, mapper, "MapInput_02", "MapResult_02")
-    RunRemotePyScript("thumm03", 31728, mapper, "MapInput_03", "MapResult_03")
-    RunRemotePyScript("thumm04", 31728, mapper, "MapInput_04", "MapResult_04")
+    #RunRemotePyScript("thumm02", 31728, mapper, "MapInput_02", "MapResult_02")
+    #RunRemotePyScript("thumm03", 31728, mapper, "MapInput_03", "MapResult_03")
+    #RunRemotePyScript("thumm04", 31728, mapper, "MapInput_04", "MapResult_04")
+    mapper_t2 = RunRemotePyScriptThread("mapper_t2", "thumm02", 31728, mapper, "MapInput_02", "MapResult_02")
+    mapper_t3 = RunRemotePyScriptThread("mapper_t3", "thumm03", 31728, mapper, "MapInput_03", "MapResult_03")
+    mapper_t4 = RunRemotePyScriptThread("mapper_t4", "thumm04", 31728, mapper, "MapInput_04", "MapResult_04")
+    mapper_t2.start()
+    mapper_t3.start()
+    mapper_t4.start()
+    mapper_t2.join()
+    mapper_t3.join()
+    mapper_t4.join()
+
     os.system("cat MapResult_02 MapResult_03 MapResult_04 > MapMidResult")
     os.remove("MapResult_02")
     os.remove("MapResult_03")
@@ -190,18 +259,38 @@ def MyMapReduce(mapper, reducer, DFSInputFile, DFSOutputFile):
     fw4.close()
     fr.close()
     print "begin send ReduceInput"
-    UploadFile("thumm02", 31728, "ReduceInput_02")
-    UploadFile("thumm03", 31728, "ReduceInput_03")
-    UploadFile("thumm04", 31728, "ReduceInput_04")
+    #UploadFile("thumm02", 31728, "ReduceInput_02")
+    #UploadFile("thumm03", 31728, "ReduceInput_03")
+    #UploadFile("thumm04", 31728, "ReduceInput_04")
+    upload_t2 = UploadFileThread("upload_t2", "thumm02", 31728, "ReduceInput_02")
+    upload_t3 = UploadFileThread("upload_t3", "thumm03", 31728, "ReduceInput_03")
+    upload_t4 = UploadFileThread("upload_t4", "thumm04", 31728, "ReduceInput_04")
+    upload_t2.start()
+    upload_t3.start()
+    upload_t4.start()
+    upload_t2.join()
+    upload_t3.join()
+    upload_t4.join()
     os.remove("ReduceInput_02")
     os.remove("ReduceInput_03")
     os.remove("ReduceInput_04")
     print "ReduceInput send complete"
     #run reducer
-    RunRemotePyScript("thumm02", 31728, reducer, "ReduceInput_02", "ReduceResult_02")
-    RunRemotePyScript("thumm03", 31728, reducer, "ReduceInput_03", "ReduceResult_03")
-    RunRemotePyScript("thumm04", 31728, reducer, "ReduceInput_04", "ReduceResult_04")
+    #RunRemotePyScript("thumm02", 31728, reducer, "ReduceInput_02", "ReduceResult_02")
+    #RunRemotePyScript("thumm03", 31728, reducer, "ReduceInput_03", "ReduceResult_03")
+    #RunRemotePyScript("thumm04", 31728, reducer, "ReduceInput_04", "ReduceResult_04")
+    reducer_t2 = RunRemotePyScriptThread("reducer_t2", "thumm02", 31728, reducer, "ReduceInput_02", "ReduceResult_02")
+    reducer_t3 = RunRemotePyScriptThread("reducer_t3", "thumm03", 31728, reducer, "ReduceInput_03", "ReduceResult_03")
+    reducer_t4 = RunRemotePyScriptThread("reducer_t4", "thumm04", 31728, reducer, "ReduceInput_04", "ReduceResult_04")
+    reducer_t2.start()
+    reducer_t3.start()
+    reducer_t4.start()
+    reducer_t2.join()
+    reducer_t3.join()
+    reducer_t4.join()
     os.system("cat ReduceResult_02 ReduceResult_03 ReduceResult_04 > ReduceResult")
     os.remove("ReduceResult_02")
     os.remove("ReduceResult_03")
     os.remove("ReduceResult_04")
+    elapsed = (time.clock() - start)
+    print "MyMapReduce Time used: %d seconds" % elapsed
